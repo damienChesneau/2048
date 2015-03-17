7785b51a98130e79010af96a0a8786979cf74687
damienChesneau <ches.damien@gmail.com>
2015-03-17 19:50:38 +0100

    Remplace with new try catch expression and File nio package use

--- src/fr/damienchesneau/ugame/logique/entitys/Direction.java
+++ src/fr/damienchesneau/ugame/logique/entitys/Direction.java
@@ -1,10 +1,28 @@
 package fr.damienchesneau.ugame.logique.entitys;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Simple énumération des directions possibles
- * @author Damien Chesneau <a href="mailto:contact@damienchesneau.fr">contact@damienchesneau.fr</a>
+ *
+ * @author Damien Chesneau
+ * <a href="mailto:contact@damienchesneau.fr">contact@damienchesneau.fr</a>
  */
 public enum Direction {
 
-    UP, DOWN, LEFT, RIGHT
+    UP, DOWN, LEFT, RIGHT;
+    
+    private static Map<String, Direction> e = new HashMap<>();
+
+    static {
+        e.put("N", Direction.UP);
+        e.put("S", Direction.DOWN);
+        e.put("W", Direction.LEFT);
+        e.put("E", Direction.RIGHT);
+    }
+
+    public static Map<String, Direction> getDirectionByString() {
+        return e;
+    }
 }
--- src/fr/damienchesneau/ugame/physique/GameSaveServiceImpl.java
+++ src/fr/damienchesneau/ugame/physique/GameSaveServiceImpl.java
@@ -5,17 +5,12 @@ import fr.damienchesneau.ugame.logique.entitys.Direction;
 import fr.damienchesneau.ugame.logique.entitys.HistoryItem;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -28,7 +23,7 @@ class GameSaveServiceImpl implements GameSaveServiceData {
     @Override
     public List<HistoryItem> getGaveByFileName(String name) throws IOException {
         String line;
-        LinkedList<HistoryItem> history = new LinkedList<>();
+        ArrayList<HistoryItem> history = new ArrayList<>();
         try (BufferedReader br = Files.newBufferedReader(Paths.get(name), Charset.forName("UTF-8"))) {
             while ((line = br.readLine()) != null) {
                 if (!line.startsWith("#")) {
@@ -39,20 +34,7 @@ class GameSaveServiceImpl implements GameSaveServiceData {
                     int index = 0;
                     if (line.startsWith("N") || line.startsWith("S") || line.startsWith("W") || line.startsWith("E")) {
                         String directionStt = line.substring(0, 1);
-                        switch (directionStt) {
-                            case "N":
-                                direction = Direction.UP;
-                                break;
-                            case "S":
-                                direction = Direction.DOWN;
-                                break;
-                            case "W":
-                                direction = Direction.LEFT;
-                                break;
-                            case "E":
-                                direction = Direction.RIGHT;
-                                break;
-                        }
+                        direction = Direction.getDirectionByString().get(directionStt);
                         index++;
                     }
                     String s = line.substring(index + 1, index + 2);
@@ -69,46 +51,45 @@ class GameSaveServiceImpl implements GameSaveServiceData {
 
     @Override
     public void saveGame(final GameService game, final String fileName) throws FileNotFoundException, IOException {
-        File file = new File(fileName);
-        FileOutputStream fos = new FileOutputStream(file);
-        List<HistoryItem> gameHistory = game.getGameHistory();
-        for (int i = 0; i < gameHistory.size(); i++) {
-            fos.write((gameHistory.get(i) + "\n").getBytes());
-            if (i == 1) {
-                fos.write(("#\n").getBytes());
-                fos.write(("#\n").getBytes());
+        try (BufferedWriter bw = Files.newBufferedWriter(Paths.get(fileName))) {
+            List<HistoryItem> gameHistory = game.getGameHistory();
+            for (int i = 0; i < gameHistory.size(); i++) {
+                bw.append(gameHistory.get(i) + "\n");
+                if (i == 1) {
+                    bw.append("#\n");
+                    bw.append("#\n");
+                }
+                bw.flush();
             }
         }
-        fos.close();
     }
 
     @Override
     public void procudeBinary(GameService game, String fileName) throws FileNotFoundException, IOException {
-        File file = new File(fileName);
-        FileOutputStream fos = new FileOutputStream(file);
-        List<HistoryItem> gameHistory = game.getGameHistory();
-        for (int i = 0; i < gameHistory.size(); i++) {
-            if (gameHistory.get(i).getStartItem() == 2) {
-                fos.write("0".getBytes());
-            } else if (gameHistory.get(i).getStartItem() == 4) {
-                fos.write("1".getBytes());
+        try (BufferedWriter bw = Files.newBufferedWriter(Paths.get(fileName))) {
+            List<HistoryItem> gameHistory = game.getGameHistory();
+            for (HistoryItem gameHistory1 : gameHistory) {
+                if (gameHistory1.getStartItem() == 2) {
+                    bw.write("0");
+                } else if (gameHistory1.getStartItem() == 4) {
+                    bw.write("1");
+                }
             }
         }
-        fos.close();
     }
 
     @Override
     public List<HistoryItem> getBinaryFileByName(String name) throws IOException {
         String line;
-        LinkedList<HistoryItem> history = new LinkedList<>();
-        FileInputStream fis = new FileInputStream(new File(name));
-        BufferedReader br = new BufferedReader(new InputStreamReader(fis, Charset.forName("UTF-8")));
-        while ((line = br.readLine()) != null) {
-            if (!line.startsWith("#")) {
-                for (int i = 0; i < line.length(); i++) {
-                    int val = Integer.valueOf(line.substring(i, i + 1));
-                    HistoryItem item = new HistoryItem(val == 1 ? 4 : 2, -1, -1, null);
-                    history.add(item);
+        ArrayList<HistoryItem> history = new ArrayList<>();
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(name), Charset.forName("UTF-8"))) {
+            while ((line = br.readLine()) != null) {
+                if (!line.startsWith("#")) {
+                    for (int i = 0; i < line.length(); i++) {
+                        int val = Integer.valueOf(line.substring(i, i + 1));
+                        HistoryItem item = new HistoryItem(val == 1 ? 4 : 2, -1, -1, null);
+                        history.add(item);
+                    }
                 }
             }
         }
